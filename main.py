251afb0a2698cc78796a29f4cd8fef056402e6a9
wjy <2240372338@qq.com>
2025-04-08 23:50:02 +0800

    git commit -am "修改问候语为Everyone" git pull

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # main.py
 def greet(name):
 print(f"Hello, {name}!")
+
 def goodbye(name):
 print(f"Goodbye, {name}!")
 # greet("GitHub")
 goodbye("GitHub")
 
-# greet("GitHub and VSCode")
-# greet("Everyone")
 
 # 修改后的内容：
 greet("GitHub and VSCode, Everyone")
